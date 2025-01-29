35e6a62c658897e32bf48aa5004e48227a221829
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-29 13:02:19 -0300

    Feature: Adiciona a entity livro

--- src/entities/Livro.ts
+++ src/entities/Livro.ts
@@ -1,5 +1,32 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 class Livro {
+    @PrimaryGeneratedColumn()
+     id: number
+ 
+     @Column()
+     title: string
+ 
+     @Column()
+     description: string
+ 
+     @Column()
+     publication_date: Date
+ 
+     @Column()
+     isbn: string
+ 
+     @Column()
+     page_count: number
+ 
+     @Column()
+     language: string
+ 
+     @Column()
+     created_at: Date
+ 
+     @Column()
+     updated_at: Date
  
 }
 
